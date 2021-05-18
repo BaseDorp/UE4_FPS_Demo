72e803b6d7067882bc2d39090b704a746a167865
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2021-05-18 01:22:06 -0500

    Init Commit

---
+++ FirstPersonDemo/Intermediate/Build/Win64/FirstPersonDemoEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "D:/Epic Games/UnrealProjects/FirstPersonDemo/Intermediate/Build/Win64/FirstPersonDemoEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ FirstPersonDemo/Source/FirstPersonDemo/FirstPersonDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstPersonDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPERSONDEMO_API AFirstPersonDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

0607b2c965b5bb247fd1e82640e5c1b4cc216eb4
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2021-05-26 00:47:12 -0500

    Adds Default Gamemode and Character

--- FirstPersonDemo/Intermediate/Build/Win64/FirstPersonDemoEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ FirstPersonDemo/Intermediate/Build/Win64/FirstPersonDemoEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,1 +1,1 @@
-#include "D:/Epic Games/UnrealProjects/FirstPersonDemo/Intermediate/Build/Win64/FirstPersonDemoEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "D:/Epic Games/UnrealProjects/UE4_FPS_Demo/FirstPersonDemo/Intermediate/Build/Win64/FirstPersonDemoEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- FirstPersonDemo/Intermediate/Build/Win64/UE4Editor/Inc/FirstPersonDemo/FirstPersonDemo.init.gen.cpp
+++ FirstPersonDemo/Intermediate/Build/Win64/UE4Editor/Inc/FirstPersonDemo/FirstPersonDemo.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeFirstPersonDemo_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB31A5E77,
-				0x03C28648,
+				0x7837E409,
+				0xC7216AD9,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
